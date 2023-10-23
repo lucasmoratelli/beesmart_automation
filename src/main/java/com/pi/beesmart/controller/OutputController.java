7ac9fce6a88b37147da468c37a91eeed4c641fd0
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-10-23 11:26:55 -0300

    Added Pi4j framework and output controlling

--- src/main/java/com/pi/beesmart/controller/OutputController.java
+++ src/main/java/com/pi/beesmart/controller/OutputController.java
@@ -1,7 +1,8 @@
 package com.pi.beesmart.controller;
 
 import com.pi.beesmart.model.OutputDAO;
-import org.springframework.http.ResponseEntity;
+import com.pi.beesmart.model.OutputEntity;
+import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,10 +10,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class OutputController {
+    private static final GpioPinDigitalOutput[] pin = new GpioPinDigitalOutput[31];
+
 
     @GetMapping("/output/")
     public List<OutputDTO> getOutput() {
-        return new OutputConverter().toDTO(new OutputDAO().getAll());
+        OutputDAO outputDAO = new OutputDAO();
+        List<OutputEntity> outputs = outputDAO.getAll();
+
+        return new OutputConverter().toDTO(outputs);
     }
     @GetMapping("/")
     public String hello() {
@@ -21,7 +27,34 @@ public class OutputController {
 
     @PutMapping("/output/{gpio}")
     public OutputDTO putOutput(@RequestBody OutputDTO dto, @PathVariable int gpio) {
+
+        getPin(gpio).setState(dto.status);
+
         final OutputConverter converter = new OutputConverter();
         return converter.toDTO(new OutputDAO().update(converter.toEntity(dto), gpio));
+    }
+    public GpioPinDigitalOutput getPin(int pinNum) {
+        if (pin[14] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            pin[14] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_14, PinState.LOW);
+        }
+        if (pin[15] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            pin[15] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_15, PinState.LOW);
+        }
+        if (pin[23] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            pin[23] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_23, PinState.LOW);
+        }
+        if (pin[24] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            pin[24] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_24, PinState.LOW);
+        }
+        if (pin[25] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            pin[25] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_25, PinState.LOW);
+        }
+
+        return pin[pinNum];
     }
 }
