2da0bee0c6dc036ddc2f6b8fd68402b6fe194af0
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-10-25 20:58:17 -0300

    Added input controlling

--- src/main/java/com/pi/beesmart/BeesmartApplication.java
+++ src/main/java/com/pi/beesmart/BeesmartApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeesmartApplication {
-	DeviceController deviceController = new DeviceController();
 	public static void main(String[] args) throws InterruptedException {
+		DeviceController deviceController = new DeviceController();
 		SpringApplication.run(BeesmartApplication.class, args);
-
-	}
-	public void cyclic() throws InterruptedException {
-		Thread.sleep(1000);
 		deviceController.getInPin();
 	}
-
 }
