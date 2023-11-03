9889ff08dcf7f63dd7ef2775f7ef3e21f764f829
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-11-03 11:49:20 -0300

    renamed OutputConverter to DeviceConverter, implemented code upgrades and code compatibility with new db

---
+++ src/main/java/com/pi/beesmart/controller/device/DeviceConverter.java
@@ -0,0 +1,24 @@
+package com.pi.beesmart.controller.device;
+
+import com.pi.beesmart.model.device.DeviceEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeviceConverter {
+    public List<DeviceDTO> toDTO(List<DeviceEntity> entities) {
+
+        return entities //
+                .stream() //
+                .map(entity -> new DeviceDTO(entity.id, entity.gpio, entity.name, entity.type, entity.value)) //
+                .collect(Collectors.toList());
+    }
+
+    public DeviceDTO toDTO(DeviceEntity entity) {
+        return new DeviceDTO(entity.id, entity.gpio, entity.name, entity.type, entity.value);
+    }
+
+    public DeviceEntity toEntity(DeviceDTO dto) {
+        return new DeviceEntity(dto.id, dto.gpio, dto.name, dto.type, dto.value);
+    }
+}
