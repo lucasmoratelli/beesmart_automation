ad092de368f01a6e62dbe8e3c60958934a1bc863
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-11-03 11:48:50 -0300

    renamed OutputController to DeviceController, implemented code upgrades and code compatibility with new db

--- src/main/java/com/pi/beesmart/controller/device/DeviceController.java
+++ src/main/java/com/pi/beesmart/controller/device/DeviceController.java
@@ -1,15 +1,12 @@
-package com.pi.beesmart.controller;
+package com.pi.beesmart.controller.device;
 
-import com.pi.beesmart.model.OutputDAO;
-import com.pi.beesmart.model.OutputEntity;
+import com.pi.beesmart.model.device.DeviceDAO;
+import com.pi.beesmart.model.device.DeviceEntity;
 import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import jdk.jfr.Frequency;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,26 +16,39 @@ public class DeviceController {
     private static final GpioPinDigitalInput[] in = new GpioPinDigitalInput[31];
 
 
-    @GetMapping("/output/")
-    public List<OutputDTO> getOutput() {
-        OutputDAO outputDAO = new OutputDAO();
-        List<OutputEntity> outputs = outputDAO.getAll();
-
-        return new OutputConverter().toDTO(outputs);
+    @GetMapping("/actuator/")
+    public List<DeviceDTO> getOutput() {
+        DeviceDAO deviceDAO = new DeviceDAO();
+        List<DeviceEntity> devices = deviceDAO.getAllActuators();
+        for (DeviceEntity device : devices) {
+            if (getOutPin(device.gpio).isHigh()) {
+                device.value = 1;
+            } else {
+                device.value = 0;
+            }
+        }
+        return new DeviceConverter().toDTO(devices);
     }
     @GetMapping("/")
     public String hello() {
         return "Hello World";
     }
 
-    @PutMapping("/output/{gpio}")
-    public OutputDTO putOutput(@RequestBody OutputDTO dto, @PathVariable int gpio) {
-
-        getOutPin(gpio).setState(dto.status);
-        System.out.println(getOutPin(gpio).getState());
-
-        final OutputConverter converter = new OutputConverter();
-        return converter.toDTO(new OutputDAO().update(converter.toEntity(dto), gpio));
+    @GetMapping("/toggle/{id}")
+    public boolean toggleAndLog(@PathVariable int id) {
+        DeviceDAO deviceDAO = new DeviceDAO();
+        DeviceConverter deviceConverter = new DeviceConverter();
+        DeviceDTO device = deviceConverter.toDTO(deviceDAO.getActuatorById(id));
+        getOutPin(device.gpio).toggle();
+        var pinState = getOutPin(device.gpio).getState();
+        int value;
+        if (pinState.isHigh()){
+            value = 1;
+        } else {
+            value = 0;
+        }
+        deviceDAO.addLog(id, device.type, value);
+        return pinState.isHigh();
     }
     public GpioPinDigitalOutput getOutPin(int pinNum) {
         if (out[15] == null) {
@@ -52,6 +62,10 @@ public class DeviceController {
         if (out[4] == null) {
             GpioController gpioPi = GpioFactory.getInstance();
             out[4] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+        }
+        if (out[5] == null) {
+            GpioController gpioPi = GpioFactory.getInstance();
+            out[5] = gpioPi.provisionDigitalOutputPin(RaspiPin.GPIO_05, PinState.LOW);
         }
 
         return out[pinNum];
