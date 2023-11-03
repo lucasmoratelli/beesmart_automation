396242f4fdf73da9610fde54ec860c4ad8197d04
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-11-03 11:47:11 -0300

    removed throw interruption

--- src/main/java/com/pi/beesmart/BeesmartApplication.java
+++ src/main/java/com/pi/beesmart/BeesmartApplication.java
@@ -1,12 +1,12 @@
 package com.pi.beesmart;
 
-import com.pi.beesmart.controller.DeviceController;
+import com.pi.beesmart.controller.device.DeviceController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeesmartApplication {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		DeviceController deviceController = new DeviceController();
 		SpringApplication.run(BeesmartApplication.class, args);
 		deviceController.getInPin();
